8e04133c7f1b829bf9f16871cdd4a95247f729ba
ronaldsg20 <roasarmientoga@unal.edu.co>
2016-10-20 22:28:26 -0500

    Sysout changes

--- test.java
+++ test.java
@@ -1,7 +1,7 @@
 public static void main(String[] args) {
  System.out.println("hello world");
  int x =25647;
- System.out.println("x es igual a "+x);
+ System.out.println("factorial de x es igual a "+facto(x));
 }
 public int facto(int x){
   return x==1?1:x*facto(x-1);
