30b853af3889410bed8cb67b9208ca647d8c6af9
ronaldsg20 <roasarmientoga@unal.edu.co>
2016-10-28 13:56:11 -0500

    add comment

--- test.java
+++ test.java
@@ -2,8 +2,10 @@ public static void main(String[] args) {
  System.out.println("hello world");
  int x =25647;
  System.out.println("factorial de x es igual a "+facto(x));
-//ronald 
+//ronald
 
+
+// Hola desde el pc de Ronald :D
 }
 public int facto(int x){
   return x==1?1:x*facto(x-1);
