929aa36f93d2feea4ac095925c65c68089432ff1
ronaldsg20 <roasarmientoga@unal.edu.co>
2016-10-28 13:32:18 -0500

    duplicar linea

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public static void main(String[] args) {
  System.out.println("hello world");
  int x =25647;
  System.out.println("factorial de x es igual a "+facto(x));
+ System.out.println("factorial de x es igual a "+facto(x));
 }
 public int facto(int x){
   return x==1?1:x*facto(x-1);
