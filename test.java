dd4fc71012fbd53afc9b1cd5cdd00a6a0173331e
davanegaspr <davanegaspr@unal.edu.co>
2016-10-28 13:47:45 -0500

    correccion de notas

--- test.java
+++ test.java
@@ -1,10 +1,8 @@
 public static void main(String[] args) {
  System.out.println("hello world");
  int x =25647;
+ System.out.println("factorial de x es igual a "+facto(x));
 
-  int y =784512;
- System.out.println("factorial de x es igual a "+facto(x));
- System.out.println("factorial de x es igual a "+facto(x));
 }
 public int facto(int x){
   return x==1?1:x*facto(x-1);
